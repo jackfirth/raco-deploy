5c94d4019e93a24f2921f4cbd41921404f2dc902
Jack Firth <jackhfirth@gmail.com>
2025-09-08 22:19:46 -0700

    Fix raco commands info

--- info.rkt
+++ info.rkt
@@ -9,7 +9,7 @@
 (define pkg-authors '(jackfirth))
 (define license '(Apache-2.0 OR MIT))
 (define raco-commands
-  '("deploy"
-    raco-deploy
-    "deploy assets to external systems"
-    50))
+  '(("deploy"
+     raco-deploy
+     "deploy assets to external systems"
+     50)))
